b4df5777a5210746880ce95dfc4ae10799a07a38
ubaidalishaikh <banubaid123@gmail.com>
2025-01-08 22:03:45 +0500

    spelling mistake

--- src/app/products/[product]/page.tsx
+++ src/app/products/[product]/page.tsx
@@ -163,7 +163,7 @@ export default async function Products({params}:{params:{product:string}}) {
             ${res.price}
           </span>
           <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-            Button
+            Add to cart
           </button>
           <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
             <svg
